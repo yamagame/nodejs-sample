c01b3ff39ca753647fdb402a60d640467c483baa
Yamaguchi Hiromitsu <yamagame@mac.com>
2021-10-27 08:39:49 +0900

    色付きテキストのサンプルを追加

---
+++ colored-log.js
@@ -0,0 +1,22 @@
+const { color } = require("./libs/colors");
+
+color.green();
+color.log("Green Color");
+
+color.red();
+color.log("Red Color");
+
+color.blue();
+color.log("Blue Color");
+
+color.yellow();
+color.log("Yellow Color");
+
+color.bold();
+color.log("Bold Text");
+
+color.inverse();
+color.log("Inverse Text");
+
+color.reset();
+color.log("Normal Color");
